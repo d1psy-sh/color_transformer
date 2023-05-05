94afb63fdf1ba97d44cb81b534d6d530b518c58a
nkirschall <niki.nice1203@gmail.com>
2023-05-05 12:11:56 +0200

    feat: modified parser and added general scheme struct

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 use std::fmt;
 use std::fs;
+use std::fs::write;
 
 use clap::Parser;
 use serde::Deserialize;
 
 fn main() {
     let infos = parse_args();
-    let nvim_scheme =
-        parse(fs::read_to_string(&infos.input_file).expect("Error while reading scheme file"));
-    println!("{:?}; {:?}", infos, nvim_scheme);
+    let scheme = parse_to_scheme(
+        fs::read_to_string(infos.input_file).expect("Unable to read file"),
+        infos.scheme_type,
+    );
+    println!("{:?}", scheme);
 }
 
 // A simple colorscheme transpiler from windows and kitty terminal themes to
@@ -53,7 +56,7 @@ fn parse_args() -> Infos {
     // here come the sane default the belong in a default method I know
     // TODO: refactor in a default method
     let mut infos = Infos {
-        input_file: "test.json".to_string(),
+        input_file: "test_data/win_test.json".to_string(),
         output_file: "out_test.json".to_string(),
         scheme_type: SchemeType::Win,
     };
@@ -75,8 +78,8 @@ fn parse_args() -> Infos {
     }
     if args.scheme_type.is_some() {
         match args.scheme_type.unwrap().as_str() {
-            "win" => infos.scheme_type = SchemeType::Win,
-            "kitty" => infos.scheme_type = SchemeType::Kitty,
+            "win" | "w" => infos.scheme_type = SchemeType::Win,
+            "kitty" | "k" => infos.scheme_type = SchemeType::Kitty,
             _ => println!(
                 "Invalid scheme type! There is:\n\n\t- \"win\"\n\t-\"kitty\"\nUsing default: {}",
                 infos.scheme_type
@@ -122,18 +125,125 @@ pub struct WinTermScheme {
 /// so this represents a scheme in general
 struct Scheme {
     // TODO: add the fields
+    white: String,
+    red: String,
+    pink: String,
+    green: String,
+    yellow: String,
+    blue: String,
+    aqua: String,
+    cyan: String,
+    purple: String,
+    violet: String,
+    orange: String,
+    brown: String,
+
+    seagreen: String,
+    turquoise: String,
+
+    foreground: String,
+    background: String,
+
+    cursor_color: String,
+    selection_background: String,
+}
+
+impl fmt::Debug for Scheme {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        // write every field in a new line for better readability
+        fmt::Display::fmt(
+            &format!(
+                r#"Scheme:
+        white: {0},
+        red: {1},
+        pink: {2},
+        green: {3},
+        yellow: {4},
+        blue: {5},
+        aqua: {6},
+        cyan: {7},
+        purple: {8},
+        violet: {9},
+        orange: {10},
+        brown: {11},
+
+        seagreen: {12},
+        turquoise: {13},
+
+        cursor_color: {14},
+        selection_background: {15},
+
+        foreground: {16},
+        background: {17},
+        "#,
+                self.white,
+                self.red,
+                self.pink,
+                self.green,
+                self.yellow,
+                self.blue,
+                self.aqua,
+                self.cyan,
+                self.purple,
+                self.violet,
+                self.orange,
+                self.brown,
+                self.seagreen,
+                self.turquoise,
+                self.cursor_color,
+                self.selection_background,
+                self.foreground,
+                self.background,
+            ),
+            f,
+        )
+    }
 }
 
 // TODO: implement those
 impl Scheme {
     fn new() -> Self {
-        Self {}
+        Self {
+            white: String::new(),
+            red: String::new(),
+            pink: String::new(),
+            green: String::new(),
+            yellow: String::new(),
+            blue: String::new(),
+            aqua: String::new(),
+            cyan: String::new(),
+            purple: String::new(),
+            violet: String::new(),
+            orange: String::new(),
+            brown: String::new(),
+
+            seagreen: String::new(),
+            turquoise: String::new(),
+
+            cursor_color: String::new(),
+            selection_background: String::new(),
+
+            foreground: String::new(),
+            background: String::new(),
+        }
     }
-    /// this function parses the scheme at hand to a nvim scheme using colorbuddy.nvim
-    // TODO: implement this (replaces the parse function or the parse func can invoke this one)
-    fn to_nvim() -> String {
-        todo!()
-    }
+    //         r#"require("colorbuddy").setup()
+    //
+    // Color.new('white',     '{0}')
+    // Color.new('red',       '{1}')
+    // Color.new('pink',      '{2}')
+    // Color.new('green',     '{3}')
+    // Color.new('yellow',    '{4}')
+    // Color.new('blue',      '{5}')
+    // Color.new('aqua',      '{6}')
+    // Color.new('cyan',      '{7}')
+    // Color.new('purple',    '{8}')
+    // Color.new('violet',    '{9}')
+    // Color.new('orange',    '{10}')
+    // Color.new('brown',     '{11}')
+    //
+    // Color.new('seagreen',  '{12}')
+    // Color.new('turquoise', '{13}')"#,
 }
 
 impl Default for Scheme {
@@ -142,82 +252,50 @@ impl Default for Scheme {
     }
 }
 
-fn parse(input: String) -> String {
-    let win_scheme: WinTermScheme =
-        serde_json::from_str(&input).expect("Error while parsing scheme file");
-
+/// this func should parse a kitty or win scheme to a general scheme
+fn parse_to_scheme(input: String, scheme_type: SchemeType) -> Scheme {
+    match scheme_type {
+        SchemeType::Win => parse_win_scheme(input),
+        SchemeType::Kitty => parse_kitty_scheme(input),
+    }
     // TODO: there are some misses in the colors so that is a thing we have to fix with like some
     // calculations and some tricks leaving it for now
-    let nvim_scheme = format!(
-        r#"require("colorbuddy").setup()
+    // TODO: add the rest of the colors
+}
 
-Color.new('white',     '{0}')
-Color.new('red',       '{1}')
-Color.new('pink',      '{2}')
-Color.new('green',     '{3}')
-Color.new('yellow',    '{4}')
-Color.new('blue',      '{5}')
-Color.new('aqua',      '{6}')
-Color.new('cyan',      '{7}')
-Color.new('purple',    '{8}')
-Color.new('violet',    '{9}')
-Color.new('orange',    '{10}')
-Color.new('brown',     '{11}')
+fn parse_kitty_scheme(input: String) -> Scheme {
+    todo!()
+}
 
-Color.new('seagreen',  '{12}')
-Color.new('turquoise', '{13}')"#,
-        win_scheme.white,
-        win_scheme.red,
-        "#aa00aa",
-        win_scheme.green,
-        win_scheme.yellow,
-        win_scheme.blue,
-        "#00ffff",
-        win_scheme.cyan,
-        win_scheme.purple,
-        "#ff00ff",
-        win_scheme.bright_red,
-        win_scheme.bright_yellow,
-        win_scheme.bright_green,
-        "#00ff00"
-    );
-    nvim_scheme
+fn parse_win_scheme(input: String) -> Scheme {
+    let win_scheme = serde_json::from_str::<WinTermScheme>(&input)
+        .expect("Error while parsing the win scheme json");
+    Scheme {
+        white: win_scheme.white,
+        red: win_scheme.red,
+        purple: win_scheme.purple,
+        blue: win_scheme.blue,
+        green: win_scheme.green,
+        yellow: win_scheme.yellow,
+        cyan: win_scheme.cyan,
+        background: win_scheme.background,
+        foreground: win_scheme.foreground,
+        selection_background: win_scheme.selection_background,
+        cursor_color: win_scheme.cursor_color,
+        orange: win_scheme.bright_yellow,
+        pink: win_scheme.bright_red,
+        aqua: win_scheme.bright_blue,
+        brown: win_scheme.bright_black,
+        seagreen: win_scheme.bright_green,
+        violet: win_scheme.bright_purple,
+        turquoise: win_scheme.bright_cyan,
+    }
 }
 
 #[cfg(test)]
 mod tests {
     #[test]
     fn parser_test() {
-        let input = r#"{
-  "name": "3024 Night",
-  "black": "\#090300",
-  "red": "\#db2d20",
-  "green": "\#01a252",
-  "yellow": "\#fded02",
-  "blue": "\#01a0e4",
-  "purple": "\#a16a94",
-  "cyan": "\#b5e4f4",
-  "white": "\#a5a2a2",
-  "brightBlack": "\#5c5855",
-  "brightRed": "\#e8bbd0",
-  "brightGreen": "\#3a3432",
-  "brightYellow": "\#4a4543",
-  "brightBlue": "\#807d7c",
-  "brightPurple": "\#d6d5d4",
-  "brightCyan": "\#cdab53",
-  "brightWhite": "\#f7f7f7",
-  "background": "\#090300",
-  "foreground": "\#a5a2a2",
-  "selectionBackground": "\#4a4543",
-  "cursorColor": "\#a5a2a2"
-}"#
-        .to_string();
-        let expected = "ahh todo".to_string();
-        let scheme = super::parse(input);
-        assert_eq!(
-            expected, scheme,
-            "These two are not matching expected is {}\n, and we got {}",
-            expected, scheme
-        )
+        todo!()
     }
 }
